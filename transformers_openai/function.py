28744f85ac297f687c7926bb351450de2139ecbe
huseinzol05 <husein.zol05@gmail.com>
2025-03-11 14:07:33 +0800

    initial support masking for pagedattention

--- transformers_openai/function.py
+++ transformers_openai/function.py
@@ -2,6 +2,40 @@ import torch
 import torch.nn.functional as F
 from typing import Optional
 
+def prefill_attention_mask_flatten(seq_lens, dtype=torch.bfloat16, causal=True):
+    masks = []
+    for l in seq_lens:
+        m = torch.ones(l, l)
+        if causal:
+            m = torch.tril(m)
+        masks.append(m)
+    total_size = sum(mask.size(0) for mask in masks)
+    combined_mask = torch.zeros(total_size, total_size, dtype=dtype)
+
+    current_pos = 0
+    for mask in masks:
+        size = mask.size(0)
+        combined_mask[current_pos:current_pos + size, current_pos:current_pos + size] = mask
+        current_pos += size
+
+    min_value = torch.finfo(dtype).min if dtype.is_floating_point else torch.iinfo(dtype).min
+    inverted_mask = torch.where(combined_mask == 1, torch.tensor(0, dtype=dtype), min_value)
+    return inverted_mask.unsqueeze(0)
+
+def step_attention_mask_flatten(seq_lens, dtype=torch.bfloat16):
+    num_queries = len(seq_lens)
+    total_kv_cache_len = sum(seq_lens)
+    neg_inf = torch.finfo(dtype).min
+    mask = torch.full((1, 1, num_queries, total_kv_cache_len), neg_inf, dtype=dtype)
+    start = 0
+    for i, length in enumerate(seq_lens):
+        end = start + length
+        causal_mask = torch.triu(torch.full((length, length), neg_inf, dtype=dtype), diagonal=1)
+        mask[:, :, i, start:end] = causal_mask[-1, :]
+        start = end
+
+    return mask
+
 def prefill_attention_mask(batch_size, max_len, lengths, device, dtype, causal=True):
     lengths = torch.tensor(lengths)
     if causal:
